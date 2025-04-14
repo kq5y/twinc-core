95ead3b7576c52fd1822edfc562155852ea07921
kq5y <tkser.edu@gmail.com>
2025-04-14 20:29:44 +0900

    refactor

--- createIdList.ts
+++ createIdList.ts
@@ -1,14 +1,14 @@
 export const createIdList = (fileContent: string, isFromKdBAlt: boolean) => {
   const idList = isFromKdBAlt
     ? fileContent
-      .split("\n")
-      .map((x) => x.replace('"', ""))
-      .map((x) => x.split(",")[0])
-      .filter((x) => x !== "科目番号")
-      .filter((x, i, self) => self.indexOf(x) === i)
+        .split("\n")
+        .map((x) => x.replace('"', ""))
+        .map((x) => x.split(",")[0])
+        .filter((x) => x !== "科目番号")
+        .filter((x, i, self) => self.indexOf(x) === i)
     : fileContent
-      .split("\n")
-      .filter((x, i, self) => self.indexOf(x) === i)
-      .map((x) => x.replace(/"/g, ""));
-  return idList.map((x) => x.trim());
+        .split("\n")
+        .filter((x, i, self) => self.indexOf(x) === i)
+        .map((x) => x.replace(/"/g, ""));
+  return idList.map((x) => x.trim()).filter((x) => x !== "");
 };
